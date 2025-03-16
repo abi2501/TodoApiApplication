232c0eed36b0afb0d836ca12bed767a0b7116ece
abi2501 <abiemailid@gmail.com>
2025-03-16 16:01:29 +0530

    todo node

--- TodoBackend/src/index.js
+++ TodoBackend/src/index.js
@@ -1,11 +1,15 @@
 import express from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
 import todoRoutes from "./routes/todoRoute.js";
 
 const app = express();
 const port = process.env.port || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
+
 app.use(express.json());
 
 app.use("/api", todoRoutes);
